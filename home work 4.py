ffc7884637b2378264e2860837fd2420710ab06c
Vasiliy <skokov.vasiliyazov@gmail.com>
2026-06-17 22:49:02 +0300

    Add files via upload

---
+++ home work 4.py
@@ -0,0 +1,20 @@
+type = str(input("Введите тип учебгого материал: "))
+price = float(input("Введите стоимость учебного материала: "))
+well = input("Введите категорию  материала: ")
+if type == "книга":
+    print(type)
+elif type == "фильм":
+    print(type)
+else:
+    print("Тип-другое")
+if price < 0:
+    print("некорректная цена")
+else:
+    print(price)
+if well == "математика":
+    print(well)
+elif well == "физика":
+    print(well)
+else:
+    print("Категория-другое")
+print("Материал добавлен: " "Тип =",type,",""Стоимость -",price,",""Категория -",well)
